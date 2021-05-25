59b392c2c51f452e52e56229f57fa38a396655bf
EC2 Default User <ec2-user@ip-172-31-33-149.ec2.internal>
2021-05-25 18:45:46 +0000

    semana10 - script

---
+++ mostranome.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+   # leitura interativa de uma variável
+   echo "Entre com seu nome:"
+   read NOME
+   # mostra o valor lido
+   echo “Boa noite, $NOME”
---
+++ subir_git.sh
@@ -0,0 +1,5 @@
+echo "Mensagem a subir"
+read MENSAGEM
+email="fagundes.cedric@gmail.com"
+usuario="fagundescedric"
+# Cedric Fagundes 0050008673
